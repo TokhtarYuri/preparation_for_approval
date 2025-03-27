739dcea0e6c1a3126213e05fad0c72ae5f7c7d1f
Tokhtar <jura.tokhtar@gmail.com>
2025-03-27 02:15:56 +0200

    last changes

--- src/components/blocks/registrationForm/index.js
+++ src/components/blocks/registrationForm/index.js
@@ -2,15 +2,20 @@ import styles from "./RegistrationForm.module.css";
 import { REGISTRATION_CONTENT } from "../../../constants/text";
 
 const RegistrationForm = () => {
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>{REGISTRATION_CONTENT.title}</h2>
-      <form className={styles.form}>
-        <input type="text" placeholder={REGISTRATION_CONTENT.phone} className={styles.input} />
-        <input type="text" placeholder={REGISTRATION_CONTENT.name} className={styles.input} />
-        <input type="email" placeholder={REGISTRATION_CONTENT.mail} className={styles.input} />
-        <button type="submit" className={styles.button}>{REGISTRATION_CONTENT.button}</button>
-      </form>
+    <div className={styles.mainContainer}>
+      <div className={styles.background}>
+        <div className={styles.container}>
+          <h2 className={styles.title}>{REGISTRATION_CONTENT.title}</h2>
+          <form className={styles.form}>
+            <input type="text" placeholder={REGISTRATION_CONTENT.phone} className={styles.input} />
+            <input type="text" placeholder={REGISTRATION_CONTENT.name} className={styles.input} />
+            <input type="email" placeholder={REGISTRATION_CONTENT.mail} className={styles.input} />
+            <button type="submit" className={styles.button}>{REGISTRATION_CONTENT.button}</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
